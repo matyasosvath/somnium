304a2810684d9c61c2d889bc79e68f242cb35282
Matyas <matyasosv@gmail.com>
2021-05-07 16:36:42 +0200

    Adding confidence interval

--- confidenceinterval.py
+++ confidenceinterval.py
@@ -38,41 +38,87 @@ class PermutationTest:
     def resample(self, x, size, replace=False):
         return np.random.choice(x, size=size, replace=replace)
 
-    def pvalue(self, iter=1000):
+    def pvalue(self, iter=1000, ci=True, ci_level=95):
+
+        # Calculate permuted distribution
         self.permute_dist = [self.test_stat(self.permutation()) for x in range(iter)]
-        #print(self.permute_dist)
+
+        # P-value
         count = sum(1 for i in self.permute_dist if i >= self.actual)
-        return np.round(count/iter, 3)
+        print(count)
+        # Confidence Interval
+        statistics = sorted(self.permute_dist)
+        #print(statistics)
+        # Trim endpoints of resampled CI
+        trim = ((1 - (ci_level/100))/2)
+        endpoints = int(trim*1000)
+        trimmed_ci = statistics[endpoints:-endpoints]
+        lower, upper = min(trimmed_ci), max(trimmed_ci)
+
+
+
+        return np.round(count/iter, 3), lower, upper
+
+
+class DiffTwoMeans(PermutationTest):
+    """
+    Significance test for the difference between two groups.
+    Makes no distributional assertion.
+    """
+    def test_stat(self, data):
+        if len(data) > 2:
+            raise TypeError(f'In case of more than two groups, test with ANOVA (see there).')
+
+        v1, v2 = data
+        return abs(v1.mean() - v2.mean())
+        # return abs(v1.mean() - v2.mean())
+
+
+
+if __name__ == '__main__':
+    x = np.random.randint(10, 15, size=30)
+    y = np.random.randint(10, 15, size=30)
+    z = np.random.randint(13, 15, size=30)
+    w = np.random.randint(16, 25, size=30)
+
+    d = DiffTwoMeans(x,y)
+    print(d.pvalue())
+    print(d.actual)
+    print(x.mean()- y.mean())
 
 
 
 
-class ConfidenceInterval:
-	def __init__(self):
-		pass
 
-	def bootstrapped(self, data):
-		pass
+## -------------
 
 
+# class ConfidenceInterval:
+# 	def __init__(self):
+# 		pass
 
-def interval(*variables, ci=95, method='bootstrap', iters=1000):
-    x,y = variables
-    means = sorted(list((np.mean(np.random.choice(x, size=len(x), replace=True)) for i in range(iters))))
-    # for i in range(iters):
-    #     x = np.random.choice(x, size=len(x), replace=True)
-    #     means.append(x.mean())
+# 	def bootstrapped(self, data):
+# 		pass
+
+
+
+# def interval(*variables, ci=95, method='bootstrap', iters=1000):
+#     x,y = variables
+#     means = sorted(list((np.mean(np.random.choice(x, size=len(x), replace=True)) for i in range(iters))))
+#     # for i in range(iters):
+#     #     x = np.random.choice(x, size=len(x), replace=True)
+#     #     means.append(x.mean())
     
 
-    # Trim endpoints of resampled CI
-    trim = ((1 - (ci/100))/2)
-    endpoints = int(trim*1000)
-    trimmed_ci = means[endpoints:-endpoints]
-    lower, upper = min(trimmed_ci), max(trimmed_ci)
+#     # Trim endpoints of resampled CI
+#     trim = ((1 - (ci/100))/2)
+#     endpoints = int(trim*1000)
+#     trimmed_ci = means[endpoints:-endpoints]
+#     lower, upper = min(trimmed_ci), max(trimmed_ci)
 
-    print(trim, endpoints, trimmed_ci, lower, upper)
+#     print(trim, endpoints, trimmed_ci, lower, upper)
 
-    # or 
-    trimmed_ci = means[25:]
+#     # or 
+#     trimmed_ci = means[25:]
 
-    return lower, upper
+#     return lower, upper
