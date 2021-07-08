2c86d89bf5b0f57402439f530dd034fa9756e5fd
matyas <matyasosv@gmail.com>
2021-07-08 13:07:17 +0000

    assumptions

--- anova.py
+++ anova.py
@@ -5,6 +5,11 @@
 
 
 
+#Homogenity of Variance, i.e. that the population variances in each group are equal. Test with Levene's Test for Homogeneity of Variances. 
+#In case it is violated, there are two tests that you can run that are applicable when the assumption of homogeneity of variances has been violated: 
+#(1) Welch F test or (2) Brown and Forsythe test. Alternatively, you could run a Kruskal-Wallis H test.
+
+
 
 class Anova:
     def __init__(self):
--- assumptions.py
+++ assumptions.py
@@ -7,26 +7,30 @@ import logging
 import unittest
 
 
-def box_whiskers():
-    pass
+# TODO
+# error handling
+# variable types
+# unittests
 
-def kurtosis():
-    pass
-
-def skew():
-    pass
-
-
-
-def normality_test(x, method=shapiro-wilk):
-
+def simple_hist(x):
+    # legyen egy histogram és a normal eloszlás pdf-je
     x = pd.DataFrame(x).hist()
     plt.savefig('normality_test'.png)
 
+def q_q(self):
+    pass
 
-    # boxs & whiskers
-    kurt = x.kurtosis()
-    skew = x.skew()
+def box_whiskers():
+    pass
+
+def kurtosis(x):
+    return pd.Series(x).kurtosis()
+
+def skew(x):
+    return pd.Series(x).skew()
+
+def normality_test(x: np.ndarray, method=shapiro-wilk) -> tuple:
+
     df = len(x) -1
 
     if method == 'shapiro-wilk':
@@ -37,9 +41,10 @@ def normality_test(x, method=shapiro-wilk):
         else:
             print(f'The normality of {x} scores was assessed. The Shapiro-Wilk tests indicated that the scores were not normally distributed (W({df}))={w}, p={p}.')
             print(f"The variable's skew is {skew} and kurtosis is {kurt}. ")
+        return (w,p)
 
     elif method == 'kolmogorov-szmirnov':
-        k,p = scipy.stats.kstest(arg, method)
+        k,p = scipy.stats.kstest(x, 'norm')
         if p >= 0.05:
             print(f'The normality of {x} scores was assessed. The Shapiro-Wilk tests indicated that the scores were normally distributed (W({df}))={w}, p={p}.')
             print(f"The variable's skew is {skew} and kurtosis is {kurt}. You can find the histogram of the variable in this folder.")
@@ -47,13 +52,14 @@ def normality_test(x, method=shapiro-wilk):
             print(f'The normality of {x} scores was assessed. The Shapiro-Wilk tests indicated that the scores were not normally distributed (W({df}))={w}, p={p}.')
             print(f"The variable's skew is {skew} and kurtosis is {kurt}. You can find the histogram of the variable in this folder. ")
     
+        return (k,p)
     else:
         raise VariableError 
 
 def homogeneity_of_variance(data):
     f,p = ss.levene(*data)
-    df_b = 
-    df_w =
+    #df_b = 
+    #df_w =
         if p >= 0.05:
             print(f'The homogeneity of variance assumption was not violated (F({df_b},{df_w})= {f}, p={p}).')
         else:
@@ -63,58 +69,9 @@ def homogeneity_of_variance(data):
 
 
 
-
-
-if __name__ == '__main__':
-    pass
-
-
-
-# Check for normality
-class Normality:
-    def __init__(self):
-        self.shapiro = shapiro
-        self.kstest = kstest
-        # self.box = box_plot
-
-
-    def q_q(self):
-        pass
-
-    def skewness(self):
-        pass
-
-    def kurtosis(self):
-        pass
-
-    def shapiro_wilk(self, *args: np.ndarray) -> tuple:
-
-        # another method 1
-        data = args[0]
-        if args[1]:
-            data = args[0] - args[1]
-        
-        # method 2
-        # try:
-        #     var1, var2 = data
-        #     data = var1 - var2
-        # except ValueError:
-        #     pass
-        return self.shapiro(data)
-
-    def kolmogorov_smirnov(self, *data: np.ndarray) -> tuple:
-        try:
-            var1, var2 = data
-            data = var1 - var2 # one sample t-test
-        except ValueError:
-            pass
-        return self.kstest(data, 'norm')
-
 if __name__ == '__main__':
     np.random.seed(42)
-    data = np.random.normal(loc=6, scale=5, size=30)
-    data2 = np.random.normal(loc=5, scale=3, size=30)
+    x = np.random.normal(loc=6, scale=5, size=30)
+    y = np.random.normal(loc=5, scale=3, size=30)
 
-    n = Normality()
-    statistic, pvalue = n.shapiro_wilk(data, data2)
-    print(statistic, pvalue)
+
