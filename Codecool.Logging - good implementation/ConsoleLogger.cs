ef4c71791c4f41aad9114d510f1454a6dfc46646
Matyi <matyasosv@gmail.com>
2022-06-15 11:47:28 +0200

    added good logger implementation

---
+++ Codecool.Logging - good implementation/ConsoleLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Codecool.Logging;
+
+namespace Codecool.MainProduct.Logging
+{
+    /// <summary>
+    /// An implementation of the <see cref="ILogger"/> interface.
+    /// Logs messages for the standard error stream of the process.
+    /// </summary>
+    public class ConsoleLogger : AbstractLogger
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsoleLogger"/> class.
+        /// </summary>
+        /// <param name="levels">Log levels.</param>
+        public ConsoleLogger(params LogLevel[] levels) : base(levels) { }
+
+        /// <inheritdoc />
+        protected override void SafeLog(LogLevel level, string message)
+        {
+            string formattedMessage = FormatMessage(level, message);
+            Console.WriteLine(formattedMessage);
+        }
+
+        private string FormatMessage(LogLevel level, string message)
+        {
+            var time = DateTime.Now.ToUniversalTime();
+            return $"{time} - {level.ToString()}: {message} ";
+        }
+    }
+}
---
+++ Codecool.Logging - good implementation/LogLevel.cs
@@ -0,0 +1,14 @@
+﻿namespace Codecool.Logging
+{
+    /// <summary>
+    /// Represents different log levels to use for logging events
+    /// with different severity.
+    /// </summary>
+    public enum LogLevel
+    {
+        INFO,
+        DEBUG,
+        WARNING,
+        ERROR,
+    }
+}
